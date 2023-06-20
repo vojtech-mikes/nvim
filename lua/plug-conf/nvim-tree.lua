6226e2cb542da563b47d9ed9226d85cdc1bbd001
Vojtěch Mikeš <vojtechmikes98@gmail.com>
2023-06-20 20:06:14 +0200

    NvimTree now closing when file is opened, finally.

--- lua/plug-conf/nvim-tree.lua
+++ lua/plug-conf/nvim-tree.lua
@@ -1,4 +1,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  actions = {
+    open_file = {
+      quit_on_open = true
+    }
+  }
+})
